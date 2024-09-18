f158ad7e7f26920fca14e7808a93f3c16d0315e0
Yukesh_Kumar <140982046+YukeshKumar2005@users.noreply.github.com>
2024-09-18 18:25:49 +0530

    Add files via upload

---
+++ DAY-08-CIPHER.cpp
@@ -0,0 +1,45 @@
+/*QUESTION-01
+
+#include <iostream>
+using namespace std;
+
+int stringLength(const string &str){
+    int count=0;
+    for(char c:str){
+        count++;
+    }
+    return count;
+}
+
+int main(){
+    string str;
+    getline(cin,str);
+    int result=stringLength(str);
+    cout<<result;
+    return 0;
+}
+*/
+#include <iostream>
+using namespace std;
+
+int countCharacter(string &str,char ch){
+    int count=0;
+    for(char c:str){
+        if(c==ch){
+            count++;
+        }
+    }
+    return count;
+}
+
+int main(){
+    string str;
+    char ch;
+    cout<<"Enter the string: ";
+    getline(cin,str);
+    cout<<"Enter the character: ";
+    cin>>ch;
+    int result=countCharacter(str,ch);
+    cout<<result<<endl;
+    return 0;
+}
