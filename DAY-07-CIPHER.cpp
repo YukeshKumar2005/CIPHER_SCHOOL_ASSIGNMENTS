2bfba06aae4fb1faed9d8a57fe64a0305026ba46
Yukesh_Kumar <140982046+YukeshKumar2005@users.noreply.github.com>
2024-09-18 18:25:13 +0530

    Add files via upload

---
+++ DAY-07-CIPHER.cpp
@@ -0,0 +1,46 @@
+/*
+//Question -01
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        // Last i elements are already sorted
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap if the element found is greater than the next element
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}*/
+
